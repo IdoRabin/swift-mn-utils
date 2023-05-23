19af92f9f96723d30b3b9feefb4a716d77c9dadd
Ido Rabin <ido@sync.me>
2023-05-23 21:46:11 +0300

    minor bug fixed in BundleEx

--- Sources/MNUtils/extensions/BundleEx.swift
+++ Sources/MNUtils/extensions/BundleEx.swift
@@ -10,11 +10,11 @@ import Foundation
 extension Bundle {
     
     var versionNumber: String? {
-        let result = infoDictionary?["CFBundleShortVersionString"] as? String
-         if (infoDictionary?.count ?? 0 == 0) {
-             // Fallback
-             result = BUILD_VERSION.versionString(formattedWith: .fullVersion)
-         }
+        var result = infoDictionary?["CFBundleShortVersionString"] as? String
+        if (infoDictionary?.count ?? 0 == 0) {
+            // Fallback
+            result = BUILD_VERSION.versionString(formattedWith: .fullVersion)
+        }
         return result
     }
     
